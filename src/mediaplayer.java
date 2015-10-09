fa298fd203c6d8a70978a68409219ea79fddcc34
miguelito20 <lauerboy1@gmail.com>
2015-10-09 13:14:09 -0400

    initializing GUI
    Initialising the JFrame

---
+++ src/mediaplayer.java
@@ -0,0 +1,16 @@
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class mediaplayer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int framewidth = 300;
+		int frameheight = 400;
+		JFrame frame = new JFrame();
+		frame.setSize(framewidth, frameheight);
+		frame.setTitle("Media Player");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}

f9b29f5a281392454d7acb4a5f9f017606c757cc
miguelito20 <lauerboy1@gmail.com>
2015-10-09 15:07:45 -0400

    Updated User Interface
    HTML based interface with Javascript embeded into the interface.
    Currently no Javascript within the application.

--- src/mediaplayer.java
+++ src/mediaplayer.java
@@ -2,15 +2,8 @@ import java.awt.FlowLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 public class mediaplayer {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int framewidth = 300;
-		int frameheight = 400;
-		JFrame frame = new JFrame();
-		frame.setSize(framewidth, frameheight);
-		frame.setTitle("Media Player");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+	public static void main(String[] args){
+		GUI window = new GUI();
+		
 	}
 }
